e772fd44fd0af9b50c9b2739d179967dfefa7f5b
shiva <vikbobinski@gmail.com>
2021-10-18 15:50:44 +0200

    Initial commit

---
+++ src/test/java/vbo/sharenurturebackend/snb/SnbApplicationTests.java
@@ -0,0 +1,13 @@
+package vbo.sharenurturebackend.snb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
